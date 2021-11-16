6f6d900d05a6928c0c4872c98c4bcb1361f681ec
Abdalla Fadl <90295968+Abdalla2030@users.noreply.github.com>
2021-11-16 22:07:13 +0200

    Update Parser.java

--- src/os/Parser.java
+++ src/os/Parser.java
@@ -19,42 +19,45 @@ public class Parser {
 
 //////////////////////////////////////////////////////////
 package command_line;
+
+import java.util.Arrays;
+
 class Parser {
 String commandName;
-String[] allArgs;
+String[] Args;
 public static String[] allCommands = {"echo","pwd","cd","1s","1s -r","mkdir",
 			              "rmdir","touch","rm","cp","cp -r","rm",
 			               "cat","clear",">",">>","help","exit" };
 
 public boolean parse(String input) {
-    String[] inputSplit = input.split(" ") ;
-    commandName = inputSplit[0];
-    for (int i = 1;i <inputSplit.length;i++){
-             allArgs[i-1] = inputSplit[i];
-       // System.out.println(inputSplit[i]);
-        
+     String[] inputSplit = input.split(" ") ;
+    commandName = inputSplit[0]; 
+    String[] newArray = new String[inputSplit.length-1]; 
+    for (int i= 1 ; i< inputSplit.length ;i++){
+        newArray[i-1] = inputSplit[i] ; 
     }
-    /*for (int i = 0;i<Args.length;i++){
-        System.out.println(Args[i]);
-    }*/
+   Args = Arrays.copyOf(newArray,newArray.length);
+   for (int i = 0 ;i <Args.length;i++){
+       System.out.println(Args[i]);
+   }
     if (commandName == "exit"){
         return false ; 
     }
     ////////////////////////////////////  
     if ( ( commandName =="pwd" || commandName =="ls" ||commandName =="ls -r" 
-         ||commandName =="help") && inputSplit.length != 0){
+         ||commandName =="help") && Args.length != 0){
         System.out.println("ERROR: " + commandName + " not take any argumnent!");
 	return false;
     }
     /////////////////////////////////////
     if ( ( commandName =="mkdir" || commandName =="cp" ||commandName =="cp -r" 
-         ||commandName =="cat" ) && inputSplit.length == 0 ){
+         ||commandName =="cat" ) && Args.length == 0 ){
         System.out.println("ERROR: " + commandName + " take at least 1 argument!");
 	return false;
     }
     /////////////////////////////////////
      if ( ( commandName =="echo" || commandName =="rmdir" ||commandName =="touch" 
-         ||commandName =="rm" ) && inputSplit.length != 1 ){
+         ||commandName =="rm" ) && Args.length != 1 ){
         System.out.println("ERROR: " + commandName + " take one argument!");
 	return false;
     }
@@ -65,9 +68,8 @@ public boolean parse(String input) {
   return commandName;
  }
  public String[] getArgs(){
-  return allArgs; 
+  return Args; 
 }
  
 }
-
 
